ff10a2d26d2c931ff346ff047656575423496533
dineug <dineug2@gmail.com>
2019-09-01 04:59:07 +0900

    showPanel false

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,11 @@
 import {storiesOf} from '@storybook/vue'
 import Demo from '../components/Demo'
 
-storiesOf('Live', module)
+storiesOf('Live', module).addParameters({
+    options: {
+      showPanel: false,
+    },
+  })
   .add('Demo', () => ({
     components: {Demo},
     template: '<Demo/>'
