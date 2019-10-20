cedcb54d7e3ed73e950b4ee0d78d9d4d98f44db6
dineug <dineug2@gmail.com>
2019-10-20 19:04:15 +0900

    mod: vuerd-core version

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,21 +3,11 @@ import {withContexts} from '@storybook/addon-contexts/vue'
 import {language} from '../src/js/contexts'
 
 import Vue from 'vue'
+import '../src/js/vuerd-core'
 import '../src/js/markedHighlight'
 import MarkDown from '../src/components/MarkDown.vue'
-import VuerdCore from 'vuerd-core'
-import TuiEditor from 'vuerd-plugin-tui.editor'
-import ERD, {Vuerd} from 'vuerd-plugin-erd'
-import 'vuerd-core/dist/vuerd-core.css'
-import 'vuerd-plugin-tui.editor/dist/vuerd-plugin-tui.editor.css'
-import 'vuerd-plugin-erd/dist/vuerd-plugin-erd.css'
-
-VuerdCore.use(TuiEditor)
-VuerdCore.use(ERD)
-Vue.use(VuerdCore)
 
 Vue.component('MarkDown', MarkDown)
-Vue.component('Vuerd', Vuerd)
 
 function loadStories() {
   const req = require.context('../src/stories', true, /\.stories\.js$/)
